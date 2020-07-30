a31300707fc25111a27d733c86d64cf3a37debe0
Abilat01 <abilat01@yandex.ru>
2020-07-30 13:05:04 +0300

    Список

--- app/src/main/java/com/abilat01/listviewpersonal/MainActivity.kt
+++ app/src/main/java/com/abilat01/listviewpersonal/MainActivity.kt
@@ -2,10 +2,20 @@ package com.abilat01.listviewpersonal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val product = Product ("Компьютер", 150.0, "Комьпьютер 16RAM? 2GB", R.drawable.mac)
+        val product2 = Product ("Машина", 950.0, "Спортиный автомобиль", R.drawable.hqdefault)
+
+        val listProduct = listOf(product, product2)
+
+        val adapter = ProductAdapter(this, listProduct)
+
+        list_view.adapter = adapter
     }
 }
--- app/src/main/java/com/abilat01/listviewpersonal/ProductAdapter.kt
+++ app/src/main/java/com/abilat01/listviewpersonal/ProductAdapter.kt
@@ -1,4 +1,24 @@
 package com.abilat01.listviewpersonal
 
-class ProductAdapter {
+import android.content.Context
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.product_item.view.*
+
+class ProductAdapter (private val mContext: Context, private val listProduct: List<Product>) :
+    ArrayAdapter<Product>(mContext, 0, listProduct) {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+val layout = LayoutInflater.from(mContext).inflate(R.layout.product_item, parent, false)
+
+        val productIn = listProduct[position]
+
+        layout.tv_main.text = productIn.namber
+        layout.tv_money.text = "$${productIn.precio}"
+        layout.imageView_main.setImageResource(productIn.image)
+
+        return layout
+    }
 }
